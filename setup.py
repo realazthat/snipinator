12a9c4102ab3fa7456fa929d794724b3de70ea1b
Azriel Fasten <realazthat@gmail.com>
2024-02-29 00:57:12 -0500

    Prepare release 1.0.2

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='snipinator',
-    version='1.0.1',
+    version='1.0.2',
     packages=find_packages(),
     description=
     'Python code snippets for markdown files, e.g READMEs, from actual (testable) code.',

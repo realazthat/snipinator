40515873192f9701f9df94457646e6b350efc553
Azriel Fasten <realazthat@gmail.com>
2024-03-02 23:18:18 -0500

    Remove extraneous line in precommit.sh.

--- scripts/precommit.sh
+++ scripts/precommit.sh
@@ -7,7 +7,6 @@ SCRIPT_DIR=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
 source "${SCRIPT_DIR}/utilities/common.sh"
 
 VENV_PATH=".cache/scripts/.venv" source "${PROJ_PATH}/scripts/utilities/ensure-venv.sh"
-
 REQS="${PROJ_PATH}/scripts/requirements-dev.txt" source "${PROJ_PATH}/scripts/utilities/ensure-reqs.sh"
 
 # pre-commit autoupdate
